225fe37793b76306ff8cbc07ed5e66baa1669766
Khalid Daghameen <35187125+daghameenk@users.noreply.github.com>
2018-01-10 10:35:10 +0200

    Update PersonRunner.java

--- src/main/java/edu/ppu/runners/PersonRunner.java
+++ src/main/java/edu/ppu/runners/PersonRunner.java
@@ -28,7 +28,7 @@ public class PersonRunner {
 		for (Person person : persons) {
 			sum += person.getAge();
 		}
-		return sum / persons.size()+100;
+		return sum / persons.size();
 	}
 
 	public int getAgeCategory(int age) {
